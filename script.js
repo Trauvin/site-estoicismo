e2ff20d3edfc6fa60dfee4ba1eafacd67d3dc391
Trauvin <mateusgalote@gmail.com>
2021-10-08 09:24:39 -0300

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+function displayDescription() {
+  const listaPensadores = document.querySelectorAll('.js-lista li');
+  const descricoes = document.querySelectorAll('.js-descricao section');
+
+  descricoes[0].classList.add('ativo');
+
+  listaPensadores.forEach((item, index) => {
+    item.addEventListener('click', function() {
+      descricoes.forEach((d) => {
+        d.classList.remove('ativo');
+      })
+      descricoes[index].classList.toggle('ativo');
+    }); 
+  });
+}
+displayDescription();
+
+
