02d0d654e045a5bdd79caa0dc8ad2a46a32d4a9c
Trauvin <mateusgalote@gmail.com>
2021-10-08 11:58:12 -0300

    mosta e esconde resposta

--- script.js
+++ script.js
@@ -15,4 +15,20 @@ function displayDescription() {
 }
 displayDescription();
 
+function mostraEsconde() {
+  const dts = document.querySelectorAll('.js-faq dt');
+  dts[0].classList.add('ativo');
+  dts[0].nextElementSibling.classList.add('ativo');
+
+  dts.forEach((dt) => {
+    dt.addEventListener('click', function() {
+      dt.classList.toggle('ativo');
+      dt.nextElementSibling.classList.toggle('ativo');
+    })
+  });
+}
+mostraEsconde();
+
+
+
 
